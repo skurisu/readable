19d16f850d590d2a8a0ce5c2fd82274ec4a662fd
Sarah Kurisu <sarah.kurisu@gmail.com>
2017-11-04 15:31:44 -0400

    add post

--- src/components/AddPostForm/component.js
+++ src/components/AddPostForm/component.js
@@ -1,21 +1,96 @@
 import React, { Component } from 'react';
-import { Container, Form, Header } from 'semantic-ui-react';
+import { Redirect } from 'react-router';
+import { Container, Form, Header, Button, Icon } from 'semantic-ui-react';
+
+/*
+  NOTE:
+  - All form items have an onChange handler to keep track of current state of each form item
+  - After submit post is clicked, object is created with the data I'd like to post
+  - Object isn't posting in createPost method's fetch
+*/
 
 class AddPostForm extends Component {
+  state = {
+    title: '',
+    body: '',
+    category: '',
+    shouldRedirect: false
+  };
+
+  createPost(postDetails) {
+    console.log(postDetails);
+    fetch('/posts', {
+      method: 'POST',
+      headers: {
+        Authorization: 'toni',
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(postDetails)
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ shouldRedirect: true });
+      });
+  }
+
+  submitPost = () => {
+    this.createPost({
+      id: 'something',
+      author: 'anonymous',
+      timestamp: Date.now(),
+      title: this.state.title,
+      body: this.state.body,
+      cateogry: this.state.category,
+      voteScore: 0,
+      deleted: false
+    });
+  };
+
+  handleTitleChange = e => {
+    this.setState({ title: e.target.value });
+  };
+
+  handleBodyChange = e => {
+    this.setState({ body: e.target.value });
+  };
+
+  handleCategoryChange = e => {
+    this.setState({ category: e.target.value });
+  };
+
   render() {
     const { categories } = this.props;
+
+    if (this.state.shouldRedirect) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <Container text>
         <Header as="h2" textAlign="center">
           Create a Post
         </Header>
         <Form>
-          <Form.Input label="Title" />
+          <Form.Input
+            label="Title"
+            onChange={this.handleTitleChange}
+            required
+          />
           <Form.TextArea
             label="Content"
+            onChange={this.handleBodyChange}
             placeholder="What do you have to say?"
+            required
           />
-          <Form.Field label="Select a category" control="select">
+          <Form.Field
+            label="Select a category"
+            onChange={this.handleCategoryChange}
+            control="select"
+            required
+          >
             {categories.map(category => (
               <option key={category.name} value={category.name}>
                 {category.name}
@@ -23,6 +98,13 @@ class AddPostForm extends Component {
             ))}
           </Form.Field>
         </Form>
+        <Button
+          color="green"
+          style={{ marginTop: 25 }}
+          onClick={this.submitPost}
+        >
+          Add Post <Icon name="plus" style={{ opacity: 1, marginLeft: 5 }} />
+        </Button>
       </Container>
     );
   }
