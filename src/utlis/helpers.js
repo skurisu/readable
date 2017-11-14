d3688505bce9ac0e293118ab8585995c9027f55c
Sarah Kurisu <sarah.kurisu@gmail.com>
2017-11-14 09:58:00 -0500

    reformat time with moment.js

--- src/utlis/helpers.js
+++ src/utlis/helpers.js
@@ -1,16 +1,7 @@
 import moment from 'moment';
 
 export function convertTime(time) {
-  const d = moment(time)
-    .utc()
-    .calendar(null, {
-      sameDay: '[Today]',
-      nextDay: '[Tomorrow]',
-      nextWeek: 'dddd',
-      lastDay: '[Yesterday]',
-      lastWeek: '[Last] dddd',
-      sameElse: 'MMMM Do YYYY, h:mm A'
-    });
+  const d = moment(time).format('MMM Do, h:mm a');
 
   return d;
 }
