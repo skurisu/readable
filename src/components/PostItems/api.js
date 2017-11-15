caa77fc4f746ab6f6a525cd97e14d92de4d77a38
Sarah Kurisu <sarah.kurisu@gmail.com>
2017-11-14 20:49:15 -0500

    inital sort by voteScore

--- src/components/PostItems/api.js
+++ src/components/PostItems/api.js
@@ -6,6 +6,17 @@ export default function() {
       return response.json();
     })
     .then(data => {
-      return data;
+      return data.sort((a, b) => b.voteScore - a.voteScore);
     });
+}
+
+export function comment(postId) {
+  const id = postId;
+  const URL = `/posts/${id}/comments`;
+
+  return fetch(URL, {
+    headers: { Authorization: 'toni' }
+  }).then(response => {
+    return response.json();
+  });
 }
