0ac1d2e78196d949ec97d01993769b5e8492f48c
Sarah Kurisu <sarah.kurisu@gmail.com>
2017-12-02 12:23:10 -0500

    change to stateless functional component

--- src/App.js
+++ src/App.js
@@ -6,28 +6,26 @@ import AddPostForm from './components/AddPostForm/index';
 import SinglePostPageWrapper from './components/SinglePostPageWrapper';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Link to="/">
-          <Button style={{ marginLeft: 14, marginTop: 14 }}>
-            <Icon name="home" />Home
-          </Button>
-        </Link>
-        <Switch>
-          <Route exact path="/add/post" render={() => <AddPostForm />} />
-          <Route exact path="/" render={() => <MainPageWrapper />} />
-          <Route exact path="/:category" render={() => <MainPageWrapper />} />
-          <Route
-            exact
-            path="/:category/:post_id"
-            render={() => <SinglePostPageWrapper />}
-          />
-        </Switch>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Link to="/">
+        <Button style={{ marginLeft: 14, marginTop: 14 }}>
+          <Icon name="home" />Home
+        </Button>
+      </Link>
+      <Switch>
+        <Route exact path="/add/post" render={() => <AddPostForm />} />
+        <Route exact path="/" render={() => <MainPageWrapper />} />
+        <Route exact path="/:category" render={() => <MainPageWrapper />} />
+        <Route
+          exact
+          path="/:category/:post_id"
+          render={() => <SinglePostPageWrapper />}
+        />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
